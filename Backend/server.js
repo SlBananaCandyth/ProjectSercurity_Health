2c623ea76dae4312d700e5b0d9d91dd5ea362741
Phu-not-Phu <danghomp@gmail.com>
2024-06-16 16:48:47 +0700

    file server.js add SSL

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,19 @@
 //SSL Certificate
-const fs = require('fs')
-const key = fs.readFileSync('./localhost/localhost.decrypted.key')
-const cert = fs.readFileSync('./localhost/localhost.crt')
+const fs = require("fs");
+const key = fs.readFileSync("./localhost/localhost.decrypted.key");
+const cert = fs.readFileSync("./localhost/localhost.crt");
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res, next) => {
-  res.status(200).send('Hello world!')
-})
+app.get("/", (req, res, next) => {
+  res.status(200).send("Hello world!");
+});
 
-const https = require('https')
-const server = https.createServer({ key, cert }, app)
+const https = require("https");
+const server = https.createServer({ key, cert }, app);
 
-const port = 3000
+const port = 3000;
 server.listen(port, () => {
-  console.log(`Server is listening on https://localhost:${port}`)
-})
+  console.log(`Server is listening on https://localhost:${port}`);
+});
