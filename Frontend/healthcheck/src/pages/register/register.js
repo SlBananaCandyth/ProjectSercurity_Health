2716a880cc9c0ed8812b474990c6815081accc70
Minhhu <62420892+tigerex@users.noreply.github.com>
2024-06-18 05:54:49 +0700

    Login and regist
    ss

--- Frontend/healthcheck/src/pages/register/register.js
+++ Frontend/healthcheck/src/pages/register/register.js
@@ -41,73 +41,60 @@ function Register() {
   return (
     <div className="container">
       <div class="register2">
-        <img class="map" alt="" src={mapBackground}></img>
-
         <div class="login-box">
-          <div class="login-box-child"></div>
+          <div class="login-box-child">
+            <b class="create-account">CREATE ACCOUNT</b>
+            <form onSubmit={submit}>
+              <div class="inputs1">
+                <div class="box">
+                  <input
+                    class="input2"
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  ></input>
+                  <img class="mail2" alt="" src={mail}></img>
+                </div>
 
-          <b class="create-account">Create account</b>
+                <div class="box">
+                  <input class="input2" placeholder="Your age"></input>
+                  <img class="age" alt="" src={age}></img>
+                </div>
 
-          <form onSubmit={submit}>
-            <div class="inputs1">
-              <div class="box">
-                <input
-                  class="input2"
-                  type="email"
-                  placeholder="Email"
-                  onChange={(e) => setEmail(e.target.value)}
-                ></input>
-                <img class="mail2" alt="" src={mail}></img>
+                <div class="box">
+                  <input
+                    class="input2"
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                  ></input>
+                  <img class="lock2" alt="" src={lock}></img>
+                </div>
+
+                <div class="box">
+                  <input class="input2" placeholder="Your height"></input>
+                  <img class="height-icon" alt="" src={height}></img>
+                </div>
+
+
+                <div class="box">
+                  <input class="input2" placeholder="Your name"></input>
+                  <img class="name" alt="" src={name}></img>
+                </div>          
+                
+                <div class="box">
+                  <input class="input2" placeholder="Your weight"></input>
+                  <img class="weight" alt="" src={weight}></img>
+                </div>
+
               </div>
-
-              <div class="box">
-                <input class="input2" placeholder="Your age"></input>
-                <img class="age" alt="" src={age}></img>
+              <div class="sign-in">
+                <button class="signup-button" type="submit">Sign up</button>
               </div>
-
-
-              
-              <div class="box">
-                <input
-                  class="input2"
-                  placeholder="Password"
-                  onChange={(e) => setPassword(e.target.value)}
-                ></input>
-                <img class="lock2" alt="" src={lock}></img>
-              </div>
-
-              <div class="box">
-                <input class="input2" placeholder="Your height"></input>
-                <img class="height-icon" alt="" src={height}></img>
-              </div>
-
-
-              <div class="box">
-                <input class="input2" placeholder="Your name"></input>
-                <img class="name" alt="" src={name}></img>
-              </div>          
-              
-              <div class="box">
-                <input class="input2" placeholder="Your weight"></input>
-                <img class="weight" alt="" src={weight}></img>
-              </div>
-            </div>{" "}
-            <div class="sign-in">
-              <div class="sign-in-child"></div>
-              <b class="sign-up" type="submit">
-                sign up
-              </b>
-            </div>
-          </form>
-
-          <b class="already-have-an-container">
-            <span class="already-have-an-container1">
-              <span>Already have an account? </span>
-              <span class="login" onClick={handleLoginClick}>
-                Login
-              </span>
-            </span>
-          </b>
+            </form>
+            <b class="already-have-an-container">
+                <span class="login" onClick={handleLoginClick}>Already have an account? </span>
+            </b>
+          </div>
         </div>
       </div>
     </div>
