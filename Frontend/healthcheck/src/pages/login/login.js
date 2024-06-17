a408c5a174718f00da4149c4b7c1cc55c274a58a
Phu-not-Phu <danghomp@gmail.com>
2024-06-17 17:46:37 +0700

    Done conflict

--- Frontend/healthcheck/src/pages/login/login.js
+++ Frontend/healthcheck/src/pages/login/login.js
@@ -1,8 +1,13 @@
 import "./login.css";
-import mapBackground from "../../icon/mapBackground.png";
-import mail from "../../icon/envelope-closed 1.svg";
-import lock from "../../icon/lock 1.svg";
+import mapBackground from "../icon/mapBackground.png";
+import mail from "../icon/envelope-closed 1.svg";
+import lock from "../icon/lock 1.svg";
+import { useNavigate } from "react-router-dom";
 
+import { useState, useEffect, useRef, useContext } from "react";
+import axios from "axios";
+import AuthContext from "../../context/authProvider";
+import { Navigate } from "react-router-dom";
 
 function Login() {
   const { setAuth } = useContext(AuthContext);
@@ -12,7 +17,6 @@ function Login() {
   const [user_email, setEmail] = useState("");
   const [encrypted_password, setPassword] = useState("");
   const [navigate, setNavaigate] = useState(false);
-  const [navigateRegister, setNavaigateRegister] = useState(false);
   const [success, setSucess] = useState(false);
   const [errMsg, setErrMsg] = useState("");
 
@@ -50,98 +54,93 @@ function Login() {
         setErrMsg("Cannot find user");
       } else if (err.response?.status === 401) {
         setErrMsg("Unauthorized");
-      }else{
+      } else {
         setErrMsg("Something went wrong");
       }
       errRef.current.focus();
     }
   };
 
+  const navigateReg = useNavigate();
+  const handleRegisterClick = () => {
+    navigateReg("../Register");
+  };
+  
   if (navigate) {
     return <Navigate to="/" />;
   }
 
-  const registerPage = () => {
-    setNavaigateRegister(true);
-  };
-
-  if (navigateRegister) {
-    return <Navigate to="/register" />;
-  }
-
   return (
-    <div className="Login">
-      <header className="App-header">
-        <div class="demo-login">
-          <img class="mapBackground" alt="" src={mapBackground}></img>
+    <div className="container">
+      <div class="demo-login">
+        <img class="mapBackground" alt="" src={mapBackground}></img>
 
-          <div class="demo-login-child"></div>
+        <div class="demo-login-child"></div>
 
-          <b class="welcome-back">Welcome back</b>
+        <b class="welcome-back">Welcome back</b>
 
-          <>
-            {success ? (
-              <section>
-                <h1>Logged in successfully</h1>
-              </section>
-            ) : (
-              <section>
-                <p
-                  ref={errRef}
-                  className={errMsg ? "errmsg" : "offscreen"}
-                  aria-live="assertive"
-                >
-                  {errMsg}
-                </p>
-              </section>
-            )}
-          </>
+        <>
+          {success ? (
+            <section>
+              <h1>Logged in successfully</h1>
+            </section>
+          ) : (
+            <section>
+              <p
+                ref={errRef}
+                className={errMsg ? "errmsg" : "offscreen"}
+                aria-live="assertive"
+              >
+                {errMsg}
+              </p>
+            </section>
+          )}
+        </>
 
-          <form onSubmit={submit}>
-            <div class="email-input">
-              <input
-                class="input"
-                type="text"
-                placeholder="Email"
-                ref={userRef}
-                autoComplete="off"
-                onChange={(e) => setEmail(e.target.value)}
-                value={user_email}
-                required
-              ></input>
-              <img class="mail" alt="" src={mail}></img>
-            </div>
+        <form onSubmit={submit}>
+          <div class="email-input">
+            <input
+              class="input"
+              type="text"
+              placeholder="Email"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setEmail(e.target.value)}
+              value={user_email}
+              required
+            ></input>
+            <img class="mail" alt="" src={mail}></img>
+          </div>
 
-            <div class="password-input">
-              <input
-                class="input"
-                type="password"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={encrypted_password}
-                required
-              ></input>
-              <img class="lock" alt="" src={lock}></img>
-            </div>
+          <div class="password-input">
+            <input
+              class="input"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={encrypted_password}
+              required
+            ></input>
+            <img class="lock" alt="" src={lock}></img>
+          </div>
 
-            <button class="signin-button" type="submit">
-              Sign in
-              {/* <div class="signin-button-child"></div>
+          <button class="signin-button" type="submit">
+            Sign in
+            {/* <div class="signin-button-child"></div>
             <b class="sign-in">sign in</b> */}
-            </button>
-          </form>
+          </button>
+        </form>
 
-          <b class="dont-have-account">
-            <span class="dont-have-an-container1">
-              <span>Don’t have an account? </span>
-              <span class="register" onClick={registerPage}>
-                Register
-              </span>
+        <b class="dont-have-account">
+          <span class="dont-have-an-container1">
+            <span>Don’t have an account? </span>
+            <span class="register" onClick={handleRegisterClick}>
+              Register
             </span>
-          </b>
-          <b class="forgot-password">Forgot password?</b>
-        </div>
-      </header>
+          </span>
+        </b>
+        <b class="forgot-password">Forgot password?</b>
+      </div>
     </div>
   );
 }
--- Frontend/healthcheck/src/pages/register/register.js
+++ Frontend/healthcheck/src/pages/register/register.js
@@ -1,11 +1,17 @@
 import "./register.css";
-import mapBackground from "../../icon/mapBackground.png";
-import mail from "../../icon/envelope-closed 1.svg";
-import lock from "../../icon/lock 1.svg";
+import mapBackground from "../icon/mapBackground.png";
+import mail from "../icon/envelope-closed 1.svg";
+import lock from "../icon/lock 1.svg";
+import name from "../icon/nameBadge.svg";
+import age from "../icon/age.svg";
+import height from "../icon/height.svg";
+import weight from "../icon/weight.svg";
 
 import { useState } from "react";
 import axios from "axios";
 import { Navigate } from "react-router-dom";
+
+import { useNavigate } from "react-router-dom";
 
 function Register() {
   const [user_email, setEmail] = useState("");
@@ -19,62 +25,86 @@ function Register() {
       user_email,
       encrypted_password,
     });
-
     console.log(respone.data);
 
     setNavaigate(true);
   };
+  const navigateHome = useNavigate();
 
+  const handleLoginClick = () => {
+    navigateHome("/");
+  };
   if (navigate) {
     return <Navigate to="/login" />;
   }
 
   return (
-    <div className="Register">
-      <header className="App-header">
-        <div class="demo-login">
-          <img class="mapBackground" alt="" src={mapBackground}></img>
+    <div className="container">
+      <div class="register2">
+        <img class="image-2-icon" alt="" src={mapBackground}></img>
 
-          <div class="demo-login-child"></div>
+        <div class="login-box">
+          <div class="login-box-child"></div>
 
-          <b class="welcome-back">Register here</b>
+          <b class="create-account">Create account</b>
 
           <form onSubmit={submit}>
-            <div class="email-input">
-              <input
-                class="input"
-                type="email"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-              ></input>
-              <img class="mail" alt="" src={mail}></img>
-            </div>
+            <div class="inputs">
+              <div class="box">
+                <input
+                  class="input"
+                  type="email"
+                  placeholder="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                ></input>
+                <img class="group-item" alt="" src={mail}></img>
+              </div>
+              <div class="box">
+                <input
+                  class="input"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                ></input>
+                <img class="login-box-item" alt="" src={lock}></img>
+              </div>
+              <div class="box">
+                <input class="input2"></input>
+                <img class="login-box-inner" alt="" src={name}></img>
+              </div>
 
-            <div class="password-input">
-              <input
-                class="input"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              ></input>
-              <img class="lock" alt="" src={lock}></img>
-            </div>
+              <div class="box">
+                <input class="input2"></input>
+                <img class="group-icon" alt="" src={age}></img>
+              </div>
 
-            <button class="signin-button" type="submit">
-              Register in
-              {/* <div class="signin-button-child"></div>
-            <b class="sign-in">sign in</b> */}
-            </button>
+              <div class="box">
+                <input class="input2"></input>
+                <img class="height-icon" alt="" src={height}></img>
+              </div>
+
+              <div class="box">
+                <input class="input2"></input>
+                <img class="login-box-child1" alt="" src={weight}></img>
+              </div>
+            </div>{" "}
+            <div class="sign-in">
+              <div class="sign-in-child"></div>
+              <b class="sign-up" type="submit">
+                sign up
+              </b>
+            </div>
           </form>
 
-          <b class="dont-have-account">
-            <span class="dont-have-an-container1">
-              <span>Don’t have an account? </span>
-              <span class="register">Register</span>
+          <b class="already-have-an-container">
+            <span class="already-have-an-container1">
+              <span>Already have an account? </span>
+              <span class="login" onClick={handleLoginClick}>
+                Login
+              </span>
             </span>
           </b>
-          <b class="forgot-password">Forgot password?</b>
         </div>
-      </header>
+      </div>
     </div>
   );
 }
