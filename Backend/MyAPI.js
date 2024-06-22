24df32e2e999f173c99bf65e43eb5aa4472048f4
Feng <115795150+SlBananaCandyth@users.noreply.github.com>
2024-06-22 14:51:28 +0700

    ffa

--- Backend/MyAPI.js
+++ Backend/MyAPI.js
@@ -13,7 +13,7 @@ var con = mysql.createConnection({
     host: "localhost",
     port: "3306",
     user: "root",
-    password: "danghomp69",
+    password: "25122003",
     insecureAuth: true,
     database: "health_user_db"
 });
